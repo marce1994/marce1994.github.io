b484ee5cbfddbcf59d1c637fe00d3be81d8d25f6
Pablo Bianco <358126+marce1994@users.noreply.github.com>
2024-06-01 15:27:45 +0000

    chore: Update Card component styles and add work experience data

---
+++ src/src/data/workExperience.ts
@@ -0,0 +1,39 @@
+interface WorkExperience {
+    company: string;
+    position: string;
+    duration: string;
+    description: string;
+}
+
+export default [
+    {
+        company: "Digital Village",
+        position: "Sr. Fullstack Developer",
+        duration: "Oct 2023 - Present",
+        description: "Working with clients to bring ideas into reality using multiple technologies (Including Vue, Node, Python and AWS Amplify)."
+    },
+    {
+        company: "Cloud(x)",
+        position: "Sr. Fullstack Developer",
+        duration: "Feb 2022 - Aug 2023",
+        description: "Worked with couple clients in the U.S. using .Net, Aurelia, Vue, and go."
+    },
+    {
+        company: "Vates",
+        position: "Fullstack Developer .Net",
+        duration: "Feb 2021 - Feb 2022",
+        description: "Worked for a client in chile with .Net core and Angular."
+    },
+    {
+        company: "Novit",
+        position: "Fullstack Developer .Net",
+        duration: "Dec 2017 - Nov 2020",
+        description: "Worked with a real state client, with .Net Framework, .Net core, Angular, MSSQL Server, and Github Actions."
+    },
+    {
+        company: "Baufest",
+        position: "Jr. Fullstack Developer .Net",
+        duration: "May 2017 - Nov 2017",
+        description: "Worked for a very important client in Argentina, with .Net Framework, Razor, Knockout.js and MSSQL Server."
+    }
+] as WorkExperience[];
