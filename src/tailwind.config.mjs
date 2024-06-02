707930bf404c6b04a97712c1eb8d84db5bcc67ff
Pablo Bianco <358126+marce1994@users.noreply.github.com>
2024-06-02 06:01:24 +0000

    updated some styles, added background image

--- src/tailwind.config.mjs
+++ src/tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', "./node_modules/flowbite/**/*.js"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"primary-color": "var(--primary-color)",
+				"secondary-color": "var(--secondary-color)",
+				
+			},
+		},
 	},
-	plugins: [],
+	plugins: [
+		require('flowbite/plugin')
+	],
 }
